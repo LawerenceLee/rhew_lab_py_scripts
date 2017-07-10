08d8ecc2a4a7ed8dc8dc578b22de8dd1eb872d27
root <root@CH4.(none)>
2017-07-10 14:32:55 -0700

    Added choice bettween USB types, like ttyS, ttyUSB

--- readtty.py
+++ readtty.py
@@ -4,8 +4,16 @@ import subprocess
 # To run me from the command line simply type readtty.
 # If permission is denied simply switch to root
 
-usb_port = raw_input("Specify port number: /dev/ttyUSB ")
+usb_type = '' 
 
-command = 'python -m serial.tools.miniterm /dev/ttyUSB{}'.format(usb_port)
+type_selection = raw_input("Select [A] ttyUSB, [B] ttyS ").upper() 
+if type_selection == 'A':
+	usb_type = 'ttyUSB'
+elif type_selection == 'B':
+	usb_type = 'ttyS'
+
+usb_port = raw_input("Specify port number: ")
+
+command = 'python -m serial.tools.miniterm /dev/tty{}{}'.format(usb_type, usb_port)
 
 subprocess.call([command], shell=True)
