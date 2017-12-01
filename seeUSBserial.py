dda13c252f8a6d039268b04052cbdb61901f9066
LawerenceLee <LawerenceLee@users.noreply.github.com>
2017-11-30 16:31:53 -0800

    updated docstrings, and placed code in main().

--- seeUSBserial.py
+++ seeUSBserial.py
@@ -1,21 +1,22 @@
-'''Prints out a Serial (tty) USB Device's product information
-
-Documentation for pyserial lib:s.html
+'''Prints out a Serial (tty) USB Device's product information.
+Documentation for pyserial https://pythonhosted.org/pyserial/tools.html
 '''
-
 import os
 
 from serial.tools.list_ports import comports
 
-usb_list = comports()
+def main():
+    usb_list = comports()
 
-os.system('cls' if os.name == 'nt' else 'clear')
+    for usb in usb_list:
+        print('Device: ', usb.device)
+        print('Location <bus>-<port>: ', usb.location)
+        print('VID: ', usb.vid)
+        print('PID: ', usb.pid)
+        print('Serial #: ', usb.serial_number)
+        print('Product String: ', usb.product)
+        print('Manufacturer: ', usb.manufacturer, '\n')
 
-for usb in usb_list:
-    print('Device: ', usb.device)
-    print('Location <bus>-<port>: ', usb.location)
-    print('VID: ', usb.vid)
-    print('PID: ', usb.pid)
-    print('Serial #: ', usb.serial_number)
-    print('Product String: ', usb.product)
-    print('Manufacturer: ', usb.manufacturer, '\n')
+if __name__ == "__main__":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    main()
